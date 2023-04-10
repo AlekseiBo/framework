3978d2acadb7761c8a1e2d1b8776fa16a6156fa1
AlekseiBo <alex.bobylev@gmail.com>
2023-04-10 13:32:29 +0200

    Data entry for prefab added

---
+++ Runtime/Toolset/DataReference/DataPrefab.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Toolset
+{
+    [CreateAssetMenu(fileName = "PrefabDataEntry", menuName = "Game Data/Prefab Entry", order = 0)]
+    public class DataPrefab : DataEntry<GameObject>
+    {
+    }
+}
