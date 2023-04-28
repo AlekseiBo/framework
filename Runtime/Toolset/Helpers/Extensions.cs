e29ad108b08f8afdae93054ffc883e5604ba9ebc
AlekseiBo <alex.bobylev@gmail.com>
2023-04-28 10:29:30 +0200

    Dictionary increment extension added

--- Runtime/Toolset/Helpers/Extensions.cs
+++ Runtime/Toolset/Helpers/Extensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Toolset
 {
@@ -20,6 +21,14 @@ namespace Toolset
         {
             if (when) set?.Invoke(self);
             return self;
+        }
+
+        public static void Increment<K, V>(this Dictionary<K, V> self, K key, V increment) where V : IConvertible
+        {
+            if (self.TryGetValue(key, out var value))
+                self[key] = (V)Convert.ChangeType(Convert.ToDouble(value) + Convert.ToDouble(increment), typeof(V));
+            else
+                self[key] = increment;
         }
     }
 }
